5a251e490473ee5229d6c891d9c09e2ac59919c9
lamprecht <lamprecht@sim.tu-darmstadt.de>
2017-12-14 11:59:54 +0100

    added markus solution

---
+++ h2/markus/pycharmproject/dmpCtl.py
@@ -0,0 +1,17 @@
+# DMP-based controller.
+#
+# DMPPARAMS is the struct containing all the parameters of the DMP.
+#
+# PSI is the vector of basis functions.
+#
+# Q and QD are the current position and velocity, respectively.
+
+import numpy as np
+
+def dmpCtl (dmpParams, psi_i, q, qd):
+    fw=np.dot(psi_i.T,dmpParams.w)
+    qdd =dmpParams.tau**2 *(dmpParams.alpha*
+        (dmpParams.beta*(dmpParams.goal-q)-
+         (qd/dmpParams.tau))+fw)
+
+    return qdd
